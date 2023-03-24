1602d5a98cd80a5fe5c5396d68691022d5401389
SergioPerera <alu0101394503@ull.edu.es>
2023-03-24 21:25:23 +0000

    BackUp

--- src/jsonModifiers/jsonColeccionUsuario.ts
+++ src/jsonModifiers/jsonColeccionUsuario.ts
@@ -3,29 +3,3 @@ import lowdb, { LowdbSync } from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 import { Usuario } from '../modelos/usuario';
 
-interface DatabaseSchema {
-  usuarios: Usuario[];
-}
-
-export class JsonColeccionUsuario extends ColeccionUsuario {
-  private usuariosDatabase: lowdb.LowdbSync<Usuario[]>;
-
-  constructor() {
-    super();
-    const adapter = new FileSync<Usuario[]>('usuarios.json');
-    this.usuariosDatabase = lowdb(adapter);
-    this.usuariosDatabase.defaults({ usuarios: [] }).write();
-  }
-
-  public registrarUsuario(usuario: Usuario): void {
-    super.registrarUsuario(usuario);
-    const usuarios = this.usuariosDatabase.get('usuarios').value();
-    if (usuarios && usuarios.length > 0) {
-      usuarios.push(usuario);
-      this.usuariosDatabase.set('usuarios', usuarios).write();
-    }
-    else {
-      this.usuariosDatabase.defaults({ usuarios: [usuario] }).write();
-    }
-  }
-}
