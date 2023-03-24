6f0d781b3793e6d14db6164cd90f74c6d53acfe1
SergioPerera <alu0101394503@ull.edu.es>
2023-03-24 20:14:54 +0000

    BackUp

--- src/colecciones/coleccionUsuario.ts
+++ src/colecciones/coleccionUsuario.ts
@@ -1,1 +1,28 @@
-// import { Usuario } from '../modelos/usuario';
+import { Usuario } from '../modelos/usuario';
+
+/**
+ * 
+ */
+export class ColeccionUsuario {
+  private usuarios: Map<number, Usuario>;
+
+  constructor() {
+    this.usuarios = new Map();
+  }
+
+  public getUsuarios(): Map<number, Usuario> {
+    return this.usuarios;
+  }
+
+  public registrarUsuario(usuario: Usuario): void {
+    this.usuarios.set(usuario.getID(), usuario);
+  }
+
+  public listarUsuarios(): void {
+    console.clear();
+    console.log('Listando usuarios...');
+    for (const usuario of this.usuarios.values()) {
+      console.log(usuario);
+    }
+  }
+}
