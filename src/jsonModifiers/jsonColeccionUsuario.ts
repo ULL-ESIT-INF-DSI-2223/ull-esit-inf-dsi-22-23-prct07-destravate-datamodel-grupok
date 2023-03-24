e260aef79e43f49b6d01be971cf4ffe158b9cc71
Brayan Jose Perez Ramos <alu0101431185@ull.edu.es>
2023-03-24 21:06:40 +0000

    backup

---
+++ src/jsonModifiers/jsonColeccionUsuario.ts
@@ -0,0 +1,27 @@
+import {ColeccionUsuario} from '../colecciones/coleccionUsuario';
+import lowdb from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
+import { Usuario } from '../modelos/usuario';
+
+export class JsonColeccionUsuario extends ColeccionUsuario {
+  private usuariosDatabase: lowdb.LowdbSync<Usuario[]>;
+
+  constructor() {
+    super();
+    const adapter = new FileSync<Usuario[]>('usuarios.json');
+    this.usuariosDatabase = lowdb(adapter);
+    this.usuariosDatabase.defaults({ usuarios: [] }).write();
+  }
+
+  public registrarUsuario(usuario: Usuario): void {
+    super.registrarUsuario(usuario);
+    const usuarios = this.usuariosDatabase.get('usuarios').value();
+    if (usuarios && usuarios.length > 0) {
+      usuarios.push(usuario);
+      this.usuariosDatabase.set('usuarios', usuarios).write();
+    }
+    else {
+      this.usuariosDatabase.defaults({ usuarios: [usuario] }).write();
+    }
+  }
+}
