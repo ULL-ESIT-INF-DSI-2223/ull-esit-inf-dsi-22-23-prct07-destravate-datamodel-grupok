61051425ce49fc375dc74a51c060d973ccfbc808
SergioPerera <alu0101394503@ull.edu.es>
2023-03-24 21:29:03 +0000

    BackUp

--- src/modelos/sistema.ts
+++ src/modelos/sistema.ts
@@ -4,7 +4,7 @@ import FileSync from 'lowdb/adapters/FileSync';
 const adapter = new FileSync('db.json');
 const db = Lowdb(adapter);
 
-const usuario1 = { nombre: 'Pepe', edad: 23 };
-const usuario2 = { nombre: 'Juan', edad: 24 };
+// const usuario1 = { nombre: 'Pepe', edad: 23 };
+// const usuario2 = { nombre: 'Juan', edad: 24 };
 
 //db.get('usuarios').push(usuario1).write();
