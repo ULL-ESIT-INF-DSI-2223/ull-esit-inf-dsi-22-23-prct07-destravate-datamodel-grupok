881eb258a8cb1648b8858d942db198a393c2226d
Brayan Jose Perez Ramos <alu0101431185@ull.edu.es>
2023-03-25 15:26:53 +0000

    BackUp

---
+++ tests/app.spec.ts
@@ -0,0 +1,11 @@
+import "mocha";
+import { expect } from "chai";
+
+import { Gestor } from "../src/app";
+
+describe("Gestor", () => {
+  it("should return true", () => {
+    const gestor = new Gestor();
+    expect(gestor).to.be.an.instanceOf(Gestor);
+  });
+})
